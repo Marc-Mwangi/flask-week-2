d40d934732f566bcf3be9afae5505ef7acac3003
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-01-27 19:49:20 +0300

    Setting up API first trial

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,11 @@ from flask import Flask
 
 #Initialization
 app = Flask(__name__)
+
+#Setting up configuration
+
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+#Avoiding circular dependancy
+from app import views
---
+++ app/config.py
@@ -0,0 +1,5 @@
+class Config:
+    '''
+    class defining objects used both in development and production
+    '''
+    news_api_base = 'https://newsapi.org/v2/everything?q=keyword&apiKey={}'
