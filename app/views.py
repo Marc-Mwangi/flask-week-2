60c42a632f83e4a110aa23f62d303e744f10afff
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-01-28 12:24:34 +0300

    Processing data to isolate relevant informatin

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from app import app
 from .request import api_key, base_url
-import requests
+import requests ,json
 from flask import render_template
 
 @app.route('/')
 def index():
     news_url= base_url+api_key
     articles= requests.get(news_url)
-    report= articles.content
-    return render_template('index.html', data= report)
+    report= json.loads(articles.content)
+    final= report['articles']
+    publish = final[0]
+    return render_template('index.html', data= final , outlet= publish)
