0c0f6fd30a896e1e08e98e56141b491d50135594
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-01-28 11:46:55 +0300

    Accessing raw api data from templatehtml

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from app import app
-from .request import api_key
+from .request import api_key, base_url
+import requests
+from flask import render_template
 
 @app.route('/')
 def index():
-    
-    return 'Hello'
+    news_url= base_url+api_key
+    articles= requests.get(news_url)
+    report= articles.content
+    return render_template('index.html', data= report)
