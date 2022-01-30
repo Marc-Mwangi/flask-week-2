c0500dcce4815209a17d2280b3f22dd27b8b5f08
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-01-30 19:48:09 +0300

    Designing dynamic routing pages and logic

--- app/views.py
+++ app/views.py
@@ -18,3 +18,26 @@ def index():
     v = m
     l= len(v)
     return render_template('index.html', data= final ,report =report, outlet= publish, v=v, l=l, length = length)
+
+@app.route('/outlet/<source>')
+def outlet(source):
+    news_url= base_url+api_key
+    articles= requests.get(news_url)
+    report= json.loads(articles.content)
+    final= report['articles']
+    publish = final[0]
+    length = len(final)
+    r=[]
+    for i in range(0,length):
+        if source == final[i]['source']['name']:
+            r.append(final[i])
+            print(r)
+
+    m=[]
+    for i in range(0,length):
+            if final[i]['source']['name'] not in m:
+                m.append(final[i]['source']['name'])
+    v = m
+    l= len(v)
+    ld =len(r)
+    return render_template('source.html', data =r , v=v, l=l, length = length, report =report, outlet= publish ,ld=ld)
